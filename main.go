cdbbd925f08045a1c006aeaa5efae75a11d6b5e9
yang <1365651552@qq.com>
2019-11-06 00:25:40 +0800

    test

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"crypto/sha256"
+)
 
 // 1.定义结构
 type Block struct {
@@ -13,13 +16,21 @@ type Block struct {
 func NewBlock(data string, prevBlockHash []byte) *Block{
 	block := Block{
 		PrevHash:prevBlockHash,
-		Hash:[]byte{},                      //todo
+		Hash:[]byte{},
 		Data:[]byte(data),
 	}
+	block.SetHash()
 	return &block
 }
 
 // 3.生成哈希
+func (block *Block)SetHash(){
+ 	// 1.拼装数据
+	blockInfo := append(block.PrevHash, block.Hash...)
+	// 2.sha256
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
 
 // 4.引入区块链
 
@@ -30,5 +41,7 @@ func NewBlock(data string, prevBlockHash []byte) *Block{
 
 func main(){
 	block := NewBlock("应用广泛",nil)
-	fmt.Printf("data:%s",block.Data)
+	fmt.Printf("当前区块哈希:%x\n",block.Hash)
+	fmt.Printf("data:%s\n",block.Data)
+
 }
