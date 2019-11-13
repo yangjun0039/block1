e904bd6e46e58103d86e7d9d0cfba4a4779e661f
yangjun0039 <1365651552@qq.com>
2019-11-13 22:06:34 +0800

    添加交易命令

--- cli.go
+++ cli.go
@@ -3,22 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
-type CLI struct{
+type CLI struct {
 	bc *BlockChain
 }
 
 const usage = `
-	addBlock --data Data         "添加区块"
-	printChain                   "打印区块链"
-	printChainR                  "反向打印区块链"
-	getBalance --address ADDRESS "获取指定地址的余额"
+	addBlock --data Data            "添加区块"
+	printChain                      "打印区块链"
+	printChainR                     "反向打印区块链"
+	getBalance --address ADDRESS    "获取指定地址的余额"
+	send FROM TO AMOUNT MINER DATA  "由FROM转AMOUNT给TO，由MINER挖矿，同时写入DATA"
 `
 
-func (cli *CLI)Run(){
+func (cli *CLI) Run() {
 	args := os.Args
-	if len(args) < 2{
+	if len(args) < 2 {
 		fmt.Printf(usage)
 		return
 	}
@@ -29,7 +31,7 @@ func (cli *CLI)Run(){
 		//添加区块
 		fmt.Println("添加区块")
 		// 确定命令有效
-		if len(args) == 4 && args[2] =="--data"{
+		if len(args) == 4 && args[2] == "--data" {
 			data := args[3]
 			cli.AddBlock(data)
 		} else {
@@ -43,13 +45,27 @@ func (cli *CLI)Run(){
 	case "getBalance":
 		//获取余额
 		fmt.Println("获取余额")
-		if len(args) == 4 && args[2] =="--address"{
+		if len(args) == 4 && args[2] == "--address" {
 			address := args[3]
-			cli.GetBalance(address )
+			cli.GetBalance(address)
 		}
+	case "send":
+		fmt.Println("转账开始")
+		// send FROM TO AMOUNT MINER DATA
+		if len(args) != 7 {
+			fmt.Println("参数个数错误，请检查")
+			fmt.Printf(usage)
+			return
+		}
+		from := args[2]
+		to := args[3]
+		//amount := args[4]
+		amount, _ := strconv.ParseFloat(args[4], 64)
+		miner := args[5]
+		data := args[6]
+		cli.Send(from, to, amount, miner, data)
 	default:
 		fmt.Println("无效的命令")
 		fmt.Printf(usage)
 	}
 }
-
--- commandLine.go
+++ commandLine.go
@@ -34,3 +34,12 @@ func (cli *CLI) GetBalance(address string) {
 	}
 	fmt.Printf("%s余额为%f", address, total)
 }
+
+func (cli *CLI) Send(from, to string, amount float64, miner, data string) {
+	fmt.Printf("from: %s\n", from)
+	fmt.Printf("to: %s\n", to)
+	fmt.Printf("amount: %f\n", amount)
+	fmt.Printf("miner: %s\n", miner)
+	fmt.Printf("data: %s\n", data)
+	// todo
+}
