133f6a1d4dc35eef307815f77aa6f8833f873db7
yang <1365651552@qq.com>
2019-11-14 23:57:39 +0800

    钱包

--- wallets.go
+++ wallets.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"io/ioutil"
+	"bytes"
+	"encoding/gob"
+)
 
 //定义一个Wallets结构，它保存所有的wallet以及他的地址
 type Wallets struct{
@@ -9,14 +14,30 @@ type Wallets struct{
 
 //创建方法
 func NewWallets() *Wallets{
+	var ws Wallets
+	ws.WalletsMap = make(map[string]*Wallet)
+	//ws := loadFile()
+	return &ws
+}
+
+func (ws *Wallets)CreateWallet() string{
 	wallet := NewWallet()
 	address := wallet.NewAddress()
-	var wallets Wallets
-	wallets.WalletsMap = make(map[string]*Wallet)
-	wallets.WalletsMap[address] = wallet
-	return &wallets
+	//var wallets Wallets
+	//wallets.WalletsMap = make(map[string]*Wallet)
+	ws.WalletsMap[address] = wallet
+	ws.saveToFile()
+	return address
 }
 
 //保存方法，把新建的wallet添加进去
+func (ws *Wallets) saveToFile(){
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	encoder.Encode(ws)
+
+	ioutil.WriteFile("wallet.dat", buffer.Bytes(), 0600)
+
+}
 
 //读取文件方法，把所有的wallet读出来
